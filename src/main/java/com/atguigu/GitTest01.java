14129502568cd0f14b2edc5435330c8effa06eab
sookie <r0uvu0r@126.com>
2022-06-14 20:35:30 +0800

    hf1 commit

--- src/main/java/com/atguigu/GitTest01.java
+++ src/main/java/com/atguigu/GitTest01.java
@@ -5,5 +5,6 @@ public class GitTest01 {
         System.out.println("hello git01");
         System.out.println("hello git02");
         System.out.println("hello git03");
+        System.out.println("hello hot_fix1");
     }
 }
