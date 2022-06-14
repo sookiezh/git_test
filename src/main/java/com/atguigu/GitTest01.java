25392800225a864e7e1823b2286eb834398cf502
sookie <r0uvu0r@126.com>
2022-06-14 20:37:17 +0800

    hf2 commit

--- src/main/java/com/atguigu/GitTest01.java
+++ src/main/java/com/atguigu/GitTest01.java
@@ -6,5 +6,6 @@ public class GitTest01 {
         System.out.println("hello git02");
         System.out.println("hello git03");
         System.out.println("hello hot_fix1");
+        System.out.println("hello hot_fix2");
     }
 }
