42d7f48cf00a76a5e2d4e20ef922a7e868638dea
sookie <r0uvu0r@126.com>
2022-06-14 20:29:51 +0800

    second commit

--- src/main/java/com/atguigu/GitTest01.java
+++ src/main/java/com/atguigu/GitTest01.java
@@ -3,5 +3,6 @@ package com.atguigu;
 public class GitTest01 {
     public static void main(String[] args) {
         System.out.println("hello git01");
+        System.out.println("hello git02");
     }
 }
